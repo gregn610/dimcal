b23b0d17673ef533324d2898a8e4d1edd2fd883f
Greg N <gregn@fastmail.com>
2016-11-27 23:39:50 +0100

    model.py tidy-up

--- src/dimcal/dcal/models.py
+++ src/dimcal/dcal/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
@@ -44,9 +37,5 @@ class DimCalendar(models.Model):
 
     calcs = property(_get_calcs)
 
-#   def get_absolute_url(self):
-#       return '/%s/%s/%s/' % (self.calendar_date.year,
-#                              self.calendar_date.month,
-#                              self.calendar_date.day
-#                              )
+
 
