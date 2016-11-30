49634e0209c13f2f17ff85e3b0bc2cda955918c2
gregn610 <gregn@fastmail.com>
2016-11-30 14:32:00 +0100

    Update hol_hr.sql

---
+++ src/dimcal/sql/incomplete/hol_hr.sql
@@ -0,0 +1,99 @@
+-- Croatia
+
+
+BEGIN;
+
+
+
+-- January 1	New Year's Day	Nova Godina	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 1
+AND   EXTRACT( MONTH FROM calendar_date) = 1
+; 
+
+-- January 6	Epiphany	Bogojavljenje, Sveta tri kralja	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 6
+AND   EXTRACT( MONTH FROM calendar_date) = 1
+; 
+
+-- Easter and the day after	Easter and Easter Monday	Uskrs i uskrsni ponedjeljak	variable date
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE calc_western_easter_mon = TRUE
+; 
+
+-- May 1	International Workers' Day	Međunarodni praznik rada	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 1
+AND   EXTRACT( MONTH FROM calendar_date) = 5
+; 
+
+-- 60 days post Easter	Corpus Christi	Tijelovo	variable date
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE calc_western_corpuschristi_thu = TRUE
+; 
+
+-- June 22	Anti-Fascist Struggle Day	Dan antifašističke borbe	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 22
+AND   EXTRACT( MONTH FROM calendar_date) = 6
+; 
+
+-- June 25	Statehood Day	Dan državnosti	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 25
+AND   EXTRACT( MONTH FROM calendar_date) = 6
+; 
+
+-- August 5	Victory and Homeland Thanksgiving Day and the Day of Croatian defenders	Dan pobjede i domovinske zahvalnosti i Dan hrvatskih branitelja	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 5
+AND   EXTRACT( MONTH FROM calendar_date) = 8
+; 
+
+-- August 15	Assumption of Mary	Velika Gospa	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 15
+AND   EXTRACT( MONTH FROM calendar_date) = 8
+; 
+
+-- October 8	Independence Day	Dan neovisnosti	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 8
+AND   EXTRACT( MONTH FROM calendar_date) = 10
+; 
+
+-- November 1	All Saints' Day	Dan svih svetih	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 1
+AND   EXTRACT( MONTH FROM calendar_date) = 11
+; 
+
+-- December 25	Christmas	Božić	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 25
+AND   EXTRACT( MONTH FROM calendar_date) = 12
+; 
+
+-- December 26	St. Stephen's Day	Prvi dan po Božiću, Sveti Stjepan, Štefanje	
+UPDATE dim_calendar
+SET hol_hr = TRUE
+WHERE EXTRACT( DAY   FROM calendar_date) = 26
+AND   EXTRACT( MONTH FROM calendar_date) = 12
+; 
+
+
+COMMIT;
+
