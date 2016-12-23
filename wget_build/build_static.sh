094b5270638c78f6cb1312b9689ecda93c341f57
Greg N <gregn@fastmail.com>
2016-12-23 03:07:41 +0100

    Travis Success Build

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 DIR=`dirname $0`
+createdb --owner=dimcaluser dimcaldb
 ${DIR}/../src/dimcal/manage.py migrate
 ${DIR}/../src/dimcal/manage.py runserver &
 echo "Waiting 7sec for the server to start"
 sleep 7s
 cd ${DIR}/ && wget -r localhost:8000/dim_calendar/
+rm localhost:8000/dim_calendar/about # This is configured on s3 with a redirect
 kill $!
