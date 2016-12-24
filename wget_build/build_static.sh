cbd35771e6eb51b2d5ba8a532cc68738537cd531
Greg N <gregn@fastmail.com>
2016-12-24 10:25:13 +0100

    Travis CI deploy

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DIR=`dirname $0`
+dropdb --if-exists dimcaldb
 createdb --owner=dimcaluser dimcaldb
 ${DIR}/../src/dimcal/manage.py migrate
 ${DIR}/../src/dimcal/manage.py runserver &
-echo "Waiting 7sec for the server to start"
-sleep 7s
-cd ${DIR}/ && wget -r -nH localhost:8000/dim_calendar/
+echo "Waiting 17sec for the server to start"
+sleep 17s
+echo "Starting crawl"
+cd ${DIR}/ && wget --recursive --no-host-directories --no-verbose localhost:8000/dim_calendar/
 rm localhost:8000/dim_calendar/about # This is configured on s3 with a redirect
 kill $!
