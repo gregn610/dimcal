4888f5b081a6a40cf147146ea17c5331be97832f
Greg N <gregn@fastmail.com>
2016-12-24 16:47:07 +0100

    use uwsgi server

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -7,7 +7,13 @@ createdb --owner=dimcaluser dimcaldb
 # Migrations use relative paths so it's important to run them from project root
 cd ${TRAVIS_BUILD_DIR}/src/dimcal/
 ./manage.py migrate
-./manage.py runserver &
+uwsgi --master                                     \
+      --socket 127.0.0.1:3031                      \
+      --module=dimcal.wsgi:application             \
+      --env DJANGO_SETTINGS_MODULE=dimcal.settings \
+      --http :8000                                 \
+      --processes 4 --threads 2                    \
+      --stats 127.0.0.1:9191                       &
 echo "Waiting 11sec for the server to start"
 sleep 11s
 echo "Starting crawl"
