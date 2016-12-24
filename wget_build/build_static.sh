138c31bac316824e9bc5802c30f6956385e4e524
Greg N <gregn@fastmail.com>
2016-12-24 19:19:18 +0100

    Travis CI

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -21,7 +21,7 @@ cd ${TRAVIS_BUILD_DIR}/wget_build/
 
 # Thanks: http://stackoverflow.com/a/11850469/266387
 ./extract_links.py | xargs -n 1 -P 8 \
-                 wget --recursive --no-host-directories --quiet localhost:8000/dim_calendar/
+                 wget --recursive --no-host-directories --quiet
 
 rm dim_calendar/about # This is configured on s3 with a redirect
 cd ${OLDDIR}
