ad63f2f84be7f920204bcee1a26b88d161e58af3
Greg N <gregn@fastmail.com>
2016-12-24 14:42:14 +0100

    Travis CI deploy

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
-DIR=`dirname $0`
+#!/bin/bash -x
+OLDDIR=`pwd`
+
 dropdb --if-exists dimcaldb
 createdb --owner=dimcaluser dimcaldb
-${DIR}/../src/dimcal/manage.py migrate
-${DIR}/../src/dimcal/manage.py runserver &
-echo "Waiting 17sec for the server to start"
-sleep 17s
+
+# Migrations use relative paths so it's important to run them from project root
+cd ${TRAVIS_BUILD_DIR}/src/dimcal/
+./manage.py migrate
+./manage.py runserver &
+echo "Waiting 11sec for the server to start"
+sleep 11s
 echo "Starting crawl"
-cd ${DIR}/ && wget --recursive --no-host-directories --no-verbose localhost:8000/dim_calendar/
-rm localhost:8000/dim_calendar/about # This is configured on s3 with a redirect
+cd ${TRAVIS_BUILD_DIR}/wget_build/
+wget --recursive --no-host-directories --no-verbose localhost:8000/dim_calendar/
+
+rm dim_calendar/about # This is configured on s3 with a redirect
 kill $!
+
+cd ${OLDDIR}
