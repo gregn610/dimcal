53615ff21ffa4d986735459bd3a779ace4dcc6fd
Greg N <gregn@fastmail.com>
2016-12-24 20:41:04 +0100

    CI

--- wget_build/build_static.sh
+++ wget_build/build_static.sh
@@ -12,7 +12,7 @@ uwsgi --master                                     \
       --module=dimcal.wsgi:application             \
       --env DJANGO_SETTINGS_MODULE=dimcal.settings \
       --http :8000                                 \
-      --processes 8 --threads 2                    \
+      --processes 2 --threads 2                    \
       --stats 127.0.0.1:9191                       &
 echo "Waiting 11sec for the server to start"
 sleep 11s
@@ -21,10 +21,9 @@ cd ${TRAVIS_BUILD_DIR}/wget_build/
 
 # Thanks: http://stackoverflow.com/a/11850469/266387
 ./extract_links.py
-./extract_links.py | xargs -n 1 -P 8 \
+./extract_links.py | xargs -n 1 -P 2 \
                  wget --recursive --no-host-directories --no-verbose
 
 rm dim_calendar/about # This is configured on s3 with a redirect
 cd ${OLDDIR}
-echo **************************************** DONE! ****************************************
-
+echo "**************************************** DONE! ****************************************"
