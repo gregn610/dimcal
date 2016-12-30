72f111562c85ae5c3e030e8d4ca13f7e857b1cea
Greg N <gregn@fastmail.com>
2016-12-30 18:24:27 +0100

    Add return codes

--- build/html/extract_links.py
+++ build/html/extract_links.py
@@ -2,6 +2,7 @@
 # Thanks : http://stackoverflow.com/a/19222808/266387
 import requests
 import lxml.html
+import sys
 
 server = 'http://localhost:8000'
 root = 'dim_calendar'
@@ -10,3 +11,8 @@ dom = lxml.html.fromstring(requests.get(server +'/'+ root).content)
 all_links = [x for x in dom.xpath('//a/@href') if 'github.com' not in x ]
 
 [print(server + l ) for l in all_links]
+
+if all_links:
+    sys.exit(0)
+else:
+    sys.exit(1)
