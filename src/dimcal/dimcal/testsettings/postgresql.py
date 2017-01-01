d9a2c3afe3963299e26980c4209d3df4d338ecb5
Greg N <gregn@fastmail.com>
2017-01-01 15:10:04 +0100

    Cherry picking

---
+++ src/dimcal/dimcal/testsettings/postgresql.py
@@ -0,0 +1,20 @@
+"""
+Django settings for testing dimcal project releases against different DBMSs.
+Import the main settings.py file nad override as appropriate
+"""
+
+from dimcal.settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES['releasedb'] = {
+        'ENGINE'   : 'django.db.backends.postgresql',
+        'USER'     : 'releaseuser',
+        'NAME'     : 'releasedb',
+        'PASSWORD' : '735341548690707',
+        'TEST': {
+                    'NAME' : 'releasetestdb',
+                },
+    }
+TEST_DIALECT='PLPGSQL'
