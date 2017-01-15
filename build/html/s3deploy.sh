798fe0155e87e748fdabde825b14a5973a48fd55
Greg N <gregn@fastmail.com>
2017-01-15 06:42:13 +0100

    parallel s3 deploy

--- build/html/s3deploy.sh
+++ build/html/s3deploy.sh
@@ -2,9 +2,9 @@
 OLDDIR=`pwd`
 
 LOAD_DIR=${TRAVIS_BUILD_DIR}/build/html/dim_calendar
-DEPLOY_TARGET_PATH=dimcalendar.gregnicol.xyz/dim_calendar
+DEPLOY_TARGET_PATH="s3://dimcalendar.gregnicol.xyz/dim_calendar"
 ls ${LOAD_DIR} -lh
-ls ${LOAD_DIR} | xargs -n 1 -P8 -I % aws s3 cp % s3://${DEPLOY_TARGET_PATH}
+ls ${LOAD_DIR} | xargs -n 1 -P8 -I % aws s3 sync % ${DEPLOY_TARGET_PATH} --content-encoding gzip --cache-control "max-age=3600"
 
 cd ${OLDDIR}
 echo "**************************************** DONE! ****************************************"
