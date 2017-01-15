288bd32afe3afde8699cf815311524ab582556f3
Greg N <gregn@fastmail.com>
2017-01-15 12:32:19 +0100

    parallel s3 deploy

--- build/html/s3deploy.sh
+++ build/html/s3deploy.sh
@@ -2,7 +2,7 @@
 OLDDIR=`pwd`
 
 LOAD_DIR=${TRAVIS_BUILD_DIR}/build/html/dim_calendar
-DEPLOY_TARGET_PATH="s3://dimcalendar.gregnicol.xyz/"
+DEPLOY_TARGET_PATH="s3://dimcalendar.gregnicol.xyz/dim_calendar/"
 aws s3 sync ${LOAD_DIR} ${DEPLOY_TARGET_PATH} --content-encoding gzip --cache-control "max-age=3600"
 
 cd ${OLDDIR}
